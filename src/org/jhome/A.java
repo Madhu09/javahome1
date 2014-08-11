fb883019e6d5436b881013ba5e554be036095166
ksilpa <silpareddy.03@gmail.com>
2014-08-11 21:09:37 +0530

    add name()

--- src/org/jhome/A.java
+++ src/org/jhome/A.java
@@ -5,6 +5,10 @@ public class A {
 	/**
 	 * @param args
 	 */
+	public void name() {
+
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Java");
